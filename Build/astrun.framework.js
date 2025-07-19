9144fc49b15d47a55f0c7992b0a45b77005d1d8c
Rsk Ktmk <kitamaki@nayug.jp>
2025-07-19 17:35:08 +0900

    テスト中

--- Build/astrun.framework.js
+++ Build/astrun.framework.js
@@ -1281,10 +1281,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7035312: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7035367: ($0) => { performance.now = function() { return $0; }; },  
- 7035415: ($0) => { performance.now = function() { return $0; }; },  
- 7035463: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7035440: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7035495: ($0) => { performance.now = function() { return $0; }; },  
+ 7035543: ($0) => { performance.now = function() { return $0; }; },  
+ 7035591: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/astrun.loader.js
+++ Build/astrun.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 87209206,
+    wasmFileSize: 87210808,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
