57df0162aa3ca8efbbb11caad3757ed911298552
Rsk Ktmk <kitamaki@nayug.jp>
2025-07-19 14:42:53 +0900

    ユーザー名取得＆表示

--- Build/astrun.framework.js
+++ Build/astrun.framework.js
@@ -1281,10 +1281,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7034080: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7034135: ($0) => { performance.now = function() { return $0; }; },  
- 7034183: ($0) => { performance.now = function() { return $0; }; },  
- 7034231: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7034416: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7034471: ($0) => { performance.now = function() { return $0; }; },  
+ 7034519: ($0) => { performance.now = function() { return $0; }; },  
+ 7034567: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
