00b82acec287853bf47054c0d76f570af4815462
Rsk Ktmk <kitamaki@nayug.jp>
2025-07-19 20:16:15 +0900

    修正

--- Build/astrun.loader.js
+++ Build/astrun.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 87228269,
+    wasmFileSize: 87228296,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
