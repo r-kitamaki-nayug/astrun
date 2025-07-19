a830ad40d3c5bc03f97ce6be781d149a31384cb7
Rsk Ktmk <kitamaki@nayug.jp>
2025-07-19 14:55:06 +0900

    名前表示修正

--- Build/astrun.framework.js
+++ Build/astrun.framework.js
@@ -1281,10 +1281,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7034176: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7034231: ($0) => { performance.now = function() { return $0; }; },  
- 7034279: ($0) => { performance.now = function() { return $0; }; },  
- 7034327: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7034208: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7034263: ($0) => { performance.now = function() { return $0; }; },  
+ 7034311: ($0) => { performance.now = function() { return $0; }; },  
+ 7034359: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
