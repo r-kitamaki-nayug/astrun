bf7704dc1d489763dd7984cb265caf5cbe6d2553
Rsk Ktmk <kitamaki@nayug.jp>
2025-07-19 14:05:22 +0900

    通信テスト

--- Build/astrun.framework.js
+++ Build/astrun.framework.js
@@ -1281,10 +1281,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7033840: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7033895: ($0) => { performance.now = function() { return $0; }; },  
- 7033943: ($0) => { performance.now = function() { return $0; }; },  
- 7033991: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7033936: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7033991: ($0) => { performance.now = function() { return $0; }; },  
+ 7034039: ($0) => { performance.now = function() { return $0; }; },  
+ 7034087: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/astrun.loader.js
+++ Build/astrun.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 87188475,
+    wasmFileSize: 87189912,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
